57939cd55eb3b2a03c1a17da6be3230c0c01ce99
tri2401 <tri2401na@gmail.com>
2022-09-23 10:00:16 -0500

    errors correctly on values less than 0

--- server/currency.js
+++ server/currency.js
@@ -5,13 +5,13 @@ const url = require('url');
 async function convert(req, res, next){
     let queryParam = url.parse(req.url,true).query; //creates json object for query parameters
     if (Object.keys(queryParam).length == 1) { //only allows for one query param
-        if (queryParam.yen != null && !isNaN(queryParam.yen)){ //converts yen to usd and checks if the value is a number
+        if (queryParam.yen != null && !isNaN(queryParam.yen) && Number(queryParam.yen) > 0){ //converts yen to usd and checks if the value is a number
             let amount =  new CC({from:"JPY", to:"USD", amount:Number(queryParam.yen)})
             amount.convert().then((response) => {
                res.send({"amount" : String(amount.convertedValue)});
             });
         }
-        else if (queryParam.us != null && !isNaN(queryParam.us)){  //converts usd to yen and checks if the value is a number
+        else if (queryParam.us != null && !isNaN(queryParam.us) && Number(queryParam.us) > 0){  //converts usd to yen and checks if the value is a number
             let amount =  new CC({from:"USD", to:"JPY", amount:Number(queryParam.us)})
             amount.convert().then((response) => {
                 res.send({"amount" : String(amount.convertedValue)});
