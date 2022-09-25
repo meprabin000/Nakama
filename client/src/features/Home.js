69dcb3798300fcb00629b21a23a1c09da0b20111
Srishti Nag <srishti.nag2@gmail.com>
2022-09-25 18:16:36 -0500

    added grid from react MUI

--- client/src/features/Home.js
+++ client/src/features/Home.js
@@ -1,8 +1,26 @@
 import React from "react";
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+
 
 const Home = (props) => {
-    return ( <div>
-        Home Page
+    return ( 
+    <div>
+    {/* Main Page Top Grid */}
+    <Grid container spacing = {2}>
+        {/* Creating the Top Bar  */}
+        <Grid item xs={12}>
+            <Container maxWidth="sm">
+                <Box sx={{ bgcolor: '#cfe8fc', height: '10vh' }} />
+            </Container>
+        </Grid>
+        {/* Creating the Main Body  */}
+        <Grid item xs = {12}>
+
+        </Grid>
+    </Grid>
+    {/* Closing Main Page Top Grid */}
     </div> );
 }
  
