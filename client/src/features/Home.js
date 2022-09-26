b25adc6ceb67b991dc7824f31dc6379fd2ffec0e
Srishti Nag <srishti.nag2@gmail.com>
2022-09-25 20:32:06 -0500

    creating main body and button navigates to currency converter

--- client/src/features/Home.js
+++ client/src/features/Home.js
@@ -2,9 +2,17 @@ import React from "react";
 import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import { useNavigate } from "react-router-dom";
 
 
 const Home = (props) => {
+    let navigate = useNavigate(); 
+    const routeChange = () =>{ 
+    let path = `/currencyConverter`; 
+    navigate(path);
+    }
     return ( 
     <div>
     {/* Main Page Top Grid */}
@@ -15,9 +23,37 @@ const Home = (props) => {
                 <Box sx={{ bgcolor: '#cfe8fc', height: '10vh' }} />
             </Container>
         </Grid>
+
         {/* Creating the Main Body  */}
         <Grid item xs = {12}>
+        <img src="/images/10966.jpg" alt="10966" width="100%" height="100%"/>
+        </Grid>
+        {/* Creating the currency converter button  */}
+        <Grid item xs = {6} alignItems="center">
+        <Stack direction="row" spacing={2}>
+        <Button variant="outlined" onClick={routeChange} >Currency Converter</Button>
+        </Stack>
+        </Grid>
 
+        <Grid item xs = {6}>
+        <Stack direction="row" spacing={2}>
+        <Button variant="outlined" href="#outlined-buttons">
+            Translator
+        </Button>
+        </Stack>
+        </Grid>
+        <Grid item xs = {6} alignItems="center">
+        <Stack direction="row" spacing={2}>
+        <Button variant="outlined">Itinerary</Button>
+        </Stack>
+        </Grid>
+
+        <Grid item xs = {6}>
+        <Stack direction="row" spacing={2}>
+        <Button variant="outlined" href="#outlined-buttons">
+            SOS
+        </Button>
+        </Stack>
         </Grid>
     </Grid>
     {/* Closing Main Page Top Grid */}
