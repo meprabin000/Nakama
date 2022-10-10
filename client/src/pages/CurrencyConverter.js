adacca8a15608509e3d51b1b57950e1a4a1b3a76
tri2401 <tri2401na@gmail.com>
2022-10-10 13:41:15 -0500

    caching done, needs better exchange rate api

--- client/src/pages/CurrencyConverter.js
+++ client/src/pages/CurrencyConverter.js
@@ -2,20 +2,86 @@ import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import { Button, FloatingLabel, Form, Spinner } from "react-bootstrap";
 import useFetch from "../customHooks/useFetch";
+import {createCache, useCache} from '@react-hook/cache'
+
+function addCache(name, url, response){
+    const data = new Response(JSON.stringify(response));
+    if ('caches' in window) {
+      caches.open(name).then((cache) => {
+        cache.put(url, data);
+      });
+    }
+  };
+
+async function getCache(name, url){
+    if (typeof caches === 'undefined') 
+        return false;
+    
+    const cacheStorage = await caches.open(name);
+    const cachedResponse = await cacheStorage.match(url);
+    
+    if (!cachedResponse || !cachedResponse.ok) {
+      return false;
+    }
+  
+    return cachedResponse.json();
+  };
 
 const CurrencyConverter = (props) => {
     const currencyOptions = ["us", "yen"]
     const [amount, setAmount] = useState(1);
     const [option, setOption] = useState("us");
-    const url = `http://localhost:3001/currency?${option}=${amount}`;
-    const {isPending, data, error} = useFetch(url);
+    const [noCache, setNoCache] = useState(false);
+    const url = `http://localhost:3001/currency?us=1`;
+
+    // const [usToYenConversionRate, setUsToYenConversionRate] = useState();
+    // const [yenToUsConversionRate, setYenToUsConversionRate] = useState();
+    const [conversionRate, setConversionRate] = useState();
     
+    const {isPending, data, error} = useFetch(url);
+
+    // let usToYenRate = 0;
+    // let yenToUsRate = 0;
+
+    if (((error == "TypeError: Failed to fetch" || (data == undefined && error == ""))) && amount > 0)
+    {
+        // if (option === 'us') {
+        //     getCache('us to yen', 'http://localhost:3001').then((item) => {
+        //         setUsToYenConversionRate(item)
+        // });
+        // }
+        // else if (option === 'yen'){
+        //     getCache('yen to us', 'http://localhost:3001').then((item) => {
+        //         console.log(item);
+        //         setYenToUsConversionRate(item)});
+        // }
+        // else {
+        // }
+        getCache('conversionRate', 'http://localhost:3001').then((item) => {
+            setConversionRate(item)
+        });
+    } else if (error != "TypeError: Failed to fetch" && (data != undefined || error != "") && amount > 0){
+        // if (option === 'us') {
+        //     usToYenRate = (Number(data.amount)/Number(amount));
+        //     const setRate1 = () => {setUsToYenConversionRate(usToYenRate)};
+        //     console.log(usToYenConversionRate);
+        //     addCache('us to yen', 'http://localhost:3001', usToYenRate);            
+        // } 
+        // else if (option === 'yen') {
+        //     yenToUsRate = (Number(amount)/Number(data.amount));
+        //     const setRate2 = () => {setYenToUsConversionRate(Number(data.amount)/Number(amount))};
+        //     addCache('yen to us', 'http://localhost:3001', yenToUsRate);
+        // }
+        const setRate = () => {setConversionRate(data.amount)};
+        addCache('conversionRate', 'http://localhost:3001', data.amount);
+    }
+
     return ( <Container>
         <h1 style={{textAlign: 'center'}}>Currency Converter</h1>
 
         {isPending && <div style={{width: "100%", textAlign: 'center'}}><Spinner animation="border" /></div>}
-        {error && <p style={{textAlign: 'center', color: 'red'}}>Please enter a valid currency amount</p>}
-        {!isPending && !error && <h5 style={{textAlign: 'center', color: 'blue'}}>{amount} {option === "us" ? "USD" : "YEN"} is equivalent to {data.amount} {option === "us" ? "YEN" : "USD"}.</h5>}
+        {(error && error != "TypeError: Failed to fetch") || amount <= 0 && <p style={{textAlign: 'center', color: 'red'}}>Please enter a valid currency amount</p>}
+        {amount > 0 && !isPending && <h5 style={{textAlign: 'center', color: 'blue'}}>{amount} {option === "us" ? "USD" : "YEN"} is equivalent to {option === "us" ? (conversionRate * amount).toFixed(2) : (amount / conversionRate).toFixed(2)} {option === "us" ? "YEN" : "USD"}.</h5>}
 
         <Form onSubmit={(e) => e.preventDefault()}>
             <Form.Group className="mb-3" controlId="currencySelect">
